ba3de242a7ae55fdf8473b0be6c130b7227b9470
Shyam Sundar <m.shyamsundar1999@gmail.com>
2026-04-29 19:48:46 +0530

    fix database url fallback

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 #DATABASE_URL = "postgresql://postgres:postgrelearning123@localhost:5432/contacts_db"
 
 engine=create_engine(DATABASE_URL)
