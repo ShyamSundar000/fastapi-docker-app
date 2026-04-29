3c3b13a936c9c1ce1f24d8fbd61d89d17bd462b4
Shyam Sundar <m.shyamsundar1999@gmail.com>
2026-04-29 20:05:27 +0530

    Added postgres db

--- database.py
+++ database.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 #DATABASE_URL = "postgresql://postgres:postgrelearning123@localhost:5432/contacts_db"
-print("DATABASE_URL =", DATABASE_URL)
 engine=create_engine(DATABASE_URL)
 
 SessionLocal=sessionmaker(bind=engine)
